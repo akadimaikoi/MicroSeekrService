1a2fc3af619302ee205da245499fa8e391294794
Rodrigo Motta <rmotta.net@gmail.com>
2023-12-27 00:49:49 -0300

    up

---
+++ myApp.js
@@ -0,0 +1,45 @@
+let express = require('express');
+let app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
